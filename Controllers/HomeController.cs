38e582cd4124d4ad49ac3d1e669a963b53b12702
jrgallentine <76459959+jrgallentine@users.noreply.github.com>
2021-02-22 00:04:08 -0500

    first commit lab 21 and 25

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegLab21.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegLab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        { 
+            return View();
+        }
+
+        public IActionResult MovieEntry(Movie movie)
+        {
+            return View(movie);
+        }
+
+        public IActionResult MovieDisplay(Movie movie)
+        {
+            //if (ModelState.IsValid)
+            //{
+                return View(movie);
+        }
+
+        public IActionResult PassageVenus()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegLab21.Models
+{
+    public class Movie
+    {
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public int year { get; set; }
+        public List<string> actors { get; set; }
+        public List<string> directors { get; set; } 
+
+
+
+    }
+}
